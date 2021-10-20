8d6969645816f8b4f297e9634503dd9cbddf1a9e
thanhnh150500 <thanhnh150500@gmail.com>
2021-10-21 02:02:02 +0700

    Add project files.

---
+++ Project PRN 211/Controllers/ReviewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project_PRN_211.Controllers
+{
+    public class ReviewController : Controller
+    {
+        // GET: Review
+        public ActionResult Review()
+        {
+            return View();
+        }
+    }
+}
---
+++ Project PRN 211/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Project_PRN_211.MvcApplication" Language="C#" %>
