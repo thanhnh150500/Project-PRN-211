2eec270e3ab39c635638929b77042cc3757387b1
thanhnh150500 <thanhnh150500@gmail.com>
2021-10-24 04:05:38 +0700

    fix front end

---
+++ ModelFirst_ConsoleApp/ModelFirst_ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ModelFirst_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
